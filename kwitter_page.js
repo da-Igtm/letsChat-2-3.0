0cdf7d4e11871740bc01709347c1da87d23fdf26
da-Igtm <85411715+da-Igtm@users.noreply.github.com>
2021-11-09 17:16:35 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCn-vb1MjZ0Iltzc4qoeo3dRHlH2f09edI",
+    authDomain: "project-94-5e1a6.firebaseapp.com",
+    projectId: "project-94-5e1a6",
+    storageBucket: "project-94-5e1a6.appspot.com",
+    messagingSenderId: "631553066548",
+    appId: "1:631553066548:web:5e831c124d1d2580c4ec61"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
